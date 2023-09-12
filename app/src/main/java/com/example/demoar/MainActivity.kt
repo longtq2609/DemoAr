cb220a6e6b657967a357f18c0cf02aefcb7cea79
longtq <longtq@mirabo-global.com>
2023-09-12 18:24:50 +0700

    update note positions

--- app/src/main/java/com/example/demoar/MainActivity.kt
+++ app/src/main/java/com/example/demoar/MainActivity.kt
@@ -9,7 +9,6 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.Node
 import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
@@ -116,159 +115,27 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-//    private fun addMultipleNodes() {
-//        val sceneView = arFragment?.arSceneView?.scene
-//        val distance = sqrt(3.0)
-//        val width = 2.0
-//
-//        val position = arrayOf(
-//            Vector3(0.0f, -1.0f, -distance.toFloat()),
-//            Vector3((3.0 * width / 4.0).toFloat(), -1.0f, (-distance / 2.0).toFloat()),
-//            Vector3((3.0 * width / 4.0).toFloat(), -1.0f, (distance / 2.0).toFloat()),
-//            Vector3(0.0f, -1.0f, distance.toFloat()),
-//            Vector3((-3.0 * width / 4.0).toFloat(), -1.0f, (distance / 2.0).toFloat()),
-//            Vector3((-3.0 * width / 4.0).toFloat(), -1.0f, (-distance / 2.0).toFloat()),
-//        )
-//
-//        val rotations = arrayOf(
-//            Quaternion(0f, 1f, 0f, 0f),
-//            Quaternion(0f, 1f, 0f, -PI.toFloat() / 2f),
-//            Quaternion(0f, 1f, 0f, -2 * PI.toFloat() / 8f),
-//            Quaternion(0f, 1f, 0f, 0f),
-//            Quaternion(0f, 1f, 0f, 2 * PI.toFloat() / 8f),
-//            Quaternion(0f, 1f, 0f, PI.toFloat() / 2f),
-//        )
-//
-//        val imageResourcesMid = arrayOf(
-//            R.drawable.img1,
-//            R.drawable.img2,
-//            R.drawable.img3,
-//            R.drawable.img4,
-//            R.drawable.img5,
-//            R.drawable.img6
-//        )
-//        val imageResourcesTop = arrayOf(
-//            R.drawable.img1,
-//            R.drawable.img2,
-//            R.drawable.img3,
-//            R.drawable.img4,
-//            R.drawable.img5,
-//            R.drawable.img6
-//        )
-//        val imageResourcesBottom = arrayOf(
-//            R.drawable.img1,
-//            R.drawable.img2,
-//            R.drawable.img3,
-//            R.drawable.img4,
-//            R.drawable.img5,
-//            R.drawable.img6
-//        )
-//
-//        val topY = (width / 2 + sqrt(2.0) / 2).toFloat()
-//        val topPositions = listOf(
-//            Vector3(0f, topY, (-distance / 2).toFloat()),
-//            Vector3((3 * width / 4).toFloat(), topY, (-distance / 4).toFloat()),
-//            Vector3((3 * width / 4).toFloat(), topY, (distance / 4).toFloat()),
-//            Vector3(0f, topY, (distance / 2).toFloat()),
-//            Vector3((-3 * width / 4).toFloat(), topY, (distance / 4).toFloat()),
-//        )
-//
-//        val topRotations = listOf(
-//            Quaternion(Vector3(Math.PI.toFloat() / 4, 0f, 0f)),
-//            Quaternion(Vector3(Math.PI.toFloat() / 4, (-Math.PI / 3).toFloat(), 0f)),
-//            Quaternion(Vector3(Math.PI.toFloat() / 4, (-2 * Math.PI / 3).toFloat(), 0f)),
-//            Quaternion(Vector3(Math.PI.toFloat() / 4, Math.PI.toFloat(), 0f)),
-//            Quaternion(Vector3(Math.PI.toFloat() / 4, (2 * Math.PI / 3).toFloat(), 0f)),
-//            Quaternion(Vector3(Math.PI.toFloat() / 4, (Math.PI / 3).toFloat(), 0f))
-//        )
-//        for (i in 0 until 6) {
-//
-//            val photoNode = Node()
-//            val imageView = ImageView(this)
-//            imageView.layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                ViewGroup.LayoutParams.MATCH_PARENT
-//            )
-//
-//            Glide.with(this)
-//                .load(imageResourcesMid[i % imageResourcesMid.size])
-//                .apply(RequestOptions().centerCrop())
-//                .override(650, 650)
-//                .into(imageView)
-//
-//            ViewRenderable.builder()
-//                .setView(this, imageView)
-//                .build()
-//                .thenAccept { viewRenderer ->
-//                    photoNode.renderable = viewRenderer
-//                    photoNode.localPosition = position[i % position.size]
-//                    photoNode.localRotation = rotations[i % rotations.size]
-//                    val anchorNode = AnchorNode()
-//                    anchorNode.parent = sceneView
-//                    anchorNode.addChild(photoNode)
-//                }
-//                .exceptionally {
-//                    Log.e(TAG, "$it")
-//                    null
-//                }
-//        }
-//
-//        for (i in 0 until 6) {
-//
-//            val photoNode = Node()
-//            val imageView = ImageView(this)
-//            imageView.layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                ViewGroup.LayoutParams.MATCH_PARENT
-//            )
-//
-//            Glide.with(this)
-//                .load(imageResourcesTop[i % imageResourcesTop.size])
-//                .apply(RequestOptions().centerCrop())
-//                .override(650, 650)
-//                .into(imageView)
-//
-//            ViewRenderable.builder()
-//                .setView(this, imageView)
-//                .build()
-//                .thenAccept { viewRenderer ->
-//                    photoNode.renderable = viewRenderer
-//                    photoNode.localPosition = topPositions[i % topPositions.size]
-//                    photoNode.localRotation = topRotations[i % topRotations.size]
-//                    val anchorNode = AnchorNode()
-//                    anchorNode.parent = sceneView
-//                    anchorNode.addChild(photoNode)
-//                }
-//                .exceptionally {
-//                    Log.e(TAG, "$it")
-//                    null
-//                }
-//        }
-//
-//
-//    }
-
     private fun addNode() {
 
         val distance = sqrt(3.0)
         val width = 2.0
 
         val positions = listOf(
-            Vector3(0f, -1f, (-distance).toFloat()),
-            Vector3((3 * width / 4).toFloat(), -1f, (-distance / 2).toFloat()),
-            Vector3((3 * width / 4).toFloat(), -1f, (distance / 2).toFloat()),
-            Vector3(0f, -1f, distance.toFloat()),
-            Vector3((-3 * width / 4).toFloat(), -1f, (distance / 2).toFloat()),
-            Vector3((-3 * width / 4).toFloat(), -1f, (-distance / 2).toFloat())
+            Vector3(0.0f, 0f, -distance.toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), 0f, (-distance / 2.0).toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), 0f, (distance / 2.0).toFloat()),
+            Vector3(0.0f, 0f, distance.toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), 0f, (distance / 2.0).toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), 0f, (-distance / 2.0).toFloat()),
         )
 
         val rotations = listOf(
             Quaternion(0f, 1f, 0f, 0f),
-            Quaternion(0f, 1f, 0f, -PI.toFloat() / 3),
-            Quaternion(0f, 1f, 0f, -2 * PI.toFloat() / 3),
-            Quaternion(0f, 1f, 0f, PI.toFloat()),
-            Quaternion(0f, 1f, 0f, 2 * PI.toFloat() / 3),
-            Quaternion(0f, 1f, 0f, PI.toFloat() / -5)
+            Quaternion(0f, 1f, 0f, -PI.toFloat() / 2f),
+            Quaternion(0f, 1f, 0f, -2 * PI.toFloat() / 8f),
+            Quaternion(0f, 1f, 0f, 0f),
+            Quaternion(0f, 1f, 0f, 2 * PI.toFloat() / 8f),
+            Quaternion(0f, 1f, 0f, PI.toFloat() / 2f),
         )
 
         val imageResourcesMid = arrayOf(
@@ -294,75 +161,113 @@ class MainActivity : AppCompatActivity() {
                 .setSizer(FixedHeightViewSizer(distance.toFloat()))
                 .build()
                 .thenAccept { viewRenderer ->
-                    val anchorNode = AnchorNode()
-                    anchorNode.renderable = viewRenderer
-                    anchorNode.parent = arFragment?.arSceneView?.scene
+                    val anchorNode = Node()
+                    anchorNode.name = "mid_node$i"
                     anchorNode.localPosition = positions[i]
-                    anchorNode.localRotation = rotations[i]
                     arFragment?.arSceneView?.scene?.addChild(anchorNode)
+                    anchorNode.localRotation = rotations[i]
+                    anchorNode.renderable = viewRenderer
                 }
                 .exceptionally {
                     Log.e(TAG, "$it")
                     null
                 }
         }
-//        val topY = (width / 2 + sqrt(2.0) / 2).toFloat()
-//        val topPositions = listOf(
-//            Vector3(0f, topY, (-distance / 2).toFloat()),
-//            Vector3((3 * width / 4).toFloat(), topY, (-distance / 4).toFloat()),
-//            Vector3((3 * width / 4).toFloat(), topY, (distance / 4).toFloat()),
-//            Vector3(0f, topY, (distance / 2).toFloat()),
-//            Vector3((-3 * width / 4).toFloat(), topY, (distance / 4).toFloat()),
-//            Vector3((-3 * width / 4).toFloat(), topY, (-distance / 4).toFloat())
-//        )
-//
-//        val topRotations = listOf(
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (Math.PI / 4).toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (Math.PI / 4).toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (Math.PI / 4).toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), Math.PI.toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (-Math.PI / 4).toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (-Math.PI / 3).toFloat())
-//        )
-//
-//        for (i in 0 until 6) {
-//
-//
-//            val planeNode = Node()
-//            planeNode.name = "top_node$i"
-//            planeNode.localPosition = topPositions[i]
-//            sceneView?.addChild(planeNode)
-//            planeNode.localRotation = topRotations[i]
-//
-//
-//        }
 
-//        val bottomY = -(width / 2 + sqrt(2.0) / 2).toFloat()
-//        val bottomPositions = listOf(
-//            Vector3(0f, bottomY, (-distance / 2).toFloat()),
-//            Vector3((3 * width / 4).toFloat(), bottomY, (-distance / 4).toFloat()),
-//            Vector3((3 * width / 4).toFloat(), bottomY, (distance / 4).toFloat()),
-//            Vector3(0f, bottomY, (distance / 2).toFloat()),
-//            Vector3((-3 * width / 4).toFloat(), bottomY, (distance / 4).toFloat()),
-//            Vector3((-3 * width / 4).toFloat(), bottomY, (-distance / 4).toFloat())
-//        )
-//
-//        val bottomRotations = listOf(
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (-Math.PI / 4).toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (-Math.PI / 4).toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (-Math.PI / 4).toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), Math.PI.toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (Math.PI / 4).toFloat()),
-//            Quaternion.axisAngle(Vector3(1f, 0f, 0f), (Math.PI / 3).toFloat())
-//        )
-//
-//        for (i in 0 until 6) {
-//            val planeNode = Node()
-//            planeNode.name = "bottom_node$i"
-//            planeNode.localPosition = bottomPositions[i]
-//            sceneView?.addChild(planeNode)
-//            planeNode.localRotation = bottomRotations[i]
-//
-//        }
+        val topY = (width / 2 + sqrt(2.0) / 2).toFloat()
+
+
+        val topPositions = listOf(
+            Vector3(0.0f, topY, -distance.toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), topY, (-distance / 2.0).toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), topY, (distance / 2.0).toFloat()),
+            Vector3(0.0f, topY, distance.toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), topY, (distance / 2.0).toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), topY, (-distance / 2.0).toFloat()),
+        )
+
+        val topRotations = listOf(
+            Quaternion(0f, 1f, PI.toFloat() / 4f, 0f),
+            Quaternion(0f, 1f, -PI.toFloat() / 4f, -PI.toFloat() / 2f),
+            Quaternion(-PI.toFloat() / 5f, 1f, -PI.toFloat() / 4f, -2 * PI.toFloat() / 8f),
+            Quaternion(0f, 1f, -PI.toFloat() / 4f, 0f),
+            Quaternion(0f, 1f, -PI.toFloat() / 4f, 2 * PI.toFloat() / 8f),
+            Quaternion(0f, 1f, -PI.toFloat() / 4f, PI.toFloat() / 2f),
+        )
+
+        for (i in 0 until 6) {
+
+            val imageView = ImageView(this)
+            Glide.with(this)
+                .load(imageResourcesMid[i % imageResourcesMid.size])
+                .apply(RequestOptions().centerCrop())
+                .into(imageView)
+
+
+            ViewRenderable.builder()
+                .setView(this, imageView)
+                .setSizer(FixedHeightViewSizer(distance.toFloat()))
+                .build()
+                .thenAccept { viewRenderer ->
+                    val anchorNode = Node()
+                    anchorNode.name = "top_node$i"
+                    anchorNode.localPosition = topPositions[i]
+                    arFragment?.arSceneView?.scene?.addChild(anchorNode)
+                    anchorNode.localRotation = topRotations[i]
+                    anchorNode.renderable = viewRenderer
+                }
+                .exceptionally {
+                    Log.e(TAG, "$it")
+                    null
+                }
+
+
+        }
+
+        val bottomY = -(width / 2 + sqrt(2.0) / 2).toFloat()
+        val bottomPositions = listOf(
+            Vector3(0.0f, bottomY, -distance.toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), bottomY, (-distance / 2.0).toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), bottomY, (distance / 2.0).toFloat()),
+            Vector3(0.0f, bottomY, distance.toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), bottomY, (distance / 2.0).toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), bottomY, (-distance / 2.0).toFloat()),
+        )
+
+        val bottomRotations = listOf(
+            Quaternion(0f, 1f, 0f, 0f),
+            Quaternion(0f, 1f, 0f, -PI.toFloat() / 2f),
+            Quaternion(0f, 1f, 0f, -2 * PI.toFloat() / 8f),
+            Quaternion(0f, 1f, 0f, 0f),
+            Quaternion(0f, 1f, 0f, 2 * PI.toFloat() / 8f),
+            Quaternion(0f, 1f, 0f, PI.toFloat() / 2f),
+        )
+
+        for (i in 0 until 6) {
+            val imageView = ImageView(this)
+            Glide.with(this)
+                .load(imageResourcesMid[i % imageResourcesMid.size])
+                .apply(RequestOptions().centerCrop())
+                .into(imageView)
+
+
+            ViewRenderable.builder()
+                .setView(this, imageView)
+                .setSizer(FixedHeightViewSizer(distance.toFloat()))
+                .build()
+                .thenAccept { viewRenderer ->
+                    val anchorNode = Node()
+                    anchorNode.name = "bottom_node$i"
+                    anchorNode.localPosition = bottomPositions[i]
+                    arFragment?.arSceneView?.scene?.addChild(anchorNode)
+                    anchorNode.localRotation = bottomRotations[i]
+                    anchorNode.renderable = viewRenderer
+                }
+                .exceptionally {
+                    Log.e(TAG, "$it")
+                    null
+                }
+
+        }
     }
 }
