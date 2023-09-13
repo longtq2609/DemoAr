bf58dbc2d3954e7904fe637b09efdb1e847bab4b
longtq <longtq@mirabo-global.com>
2023-09-13 15:44:16 +0700

    update

--- app/src/main/java/com/example/demoar/MainActivity.kt
+++ app/src/main/java/com/example/demoar/MainActivity.kt
@@ -2,32 +2,41 @@ package com.example.demoar
 
 import android.app.Activity
 import android.app.ActivityManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
+import android.view.PixelCopy
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.ar.sceneform.ArSceneView
 import com.google.ar.sceneform.Node
 import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.FixedHeightViewSizer
 import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import kotlin.math.PI
 import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity() {
 
-    private companion object {
-        const val TAG = "longtq"
-        const val MIN_OPENGL_VERSION = 3.0
-    }
-
     private var arFragment: ArFragment? = null
     private lateinit var btnCapture: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,79 +44,88 @@ class MainActivity : AppCompatActivity() {
             return
         }
         setContentView(R.layout.activity_main)
-        arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment?
-        btnCapture = findViewById(R.id.btnCapture)
+        init()
+        listener()
+    }
+
+    private fun listener() {
         btnCapture.setOnClickListener {
-            addNode()
+            takePhoto()
         }
     }
 
-//    private fun generateCacheFilename(): String {
-//        val date: String = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(Date())
-//        val cacheDir = cacheDir
-//        return cacheDir.toString() + File.separator + "IM/" + date + "_screenshot.jpg"
-//    }
-//
-//    @Throws(IOException::class)
-//    private fun saveBitmapToDisk(bitmap: Bitmap, filename: String) {
-//        val out = File(filename)
-//        if (out.parentFile?.exists() == false) {
-//            out.parentFile?.mkdirs()
-//        }
-//        try {
-//            FileOutputStream(filename).use { outputStream ->
-//                ByteArrayOutputStream().use { outputData ->
-//                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputData)
-//                    outputData.writeTo(outputStream)
-//                    outputStream.flush()
-//                    outputStream.close()
-//                }
-//            }
-//        } catch (ex: IOException) {
-//            throw IOException("Failed to save bitmap to disk", ex)
-//        }
-//    }
-//
-//    private fun takePhoto() {
-//        val filename = generateCacheFilename()
-//        val view: ArSceneView = arFragment!!.arSceneView
-//        val bitmap = Bitmap.createBitmap(
-//            view.width, view.height,
-//            Bitmap.Config.ARGB_8888
-//        )
-//
-//        val handlerThread = HandlerThread("PixelCopier")
-//        handlerThread.start()
-//        PixelCopy.request(view, bitmap, { copyResult ->
-//            if (copyResult == PixelCopy.SUCCESS) {
-//                try {
-//                    saveBitmapToDisk(bitmap, filename)
-//                    runOnUiThread {
-////                        addImageToNode(filename)
-//                    }
-//
-//                } catch (e: IOException) {
-//                    val toast = Toast.makeText(
-//                        this@MainActivity, e.toString(),
-//                        Toast.LENGTH_LONG
-//                    )
-//                    toast.show()
-//                    return@request
-//                }
-//            } else {
-//                //noop
-//            }
-//            handlerThread.quitSafely()
-//        }, Handler(handlerThread.looper))
-//
-//    }
+    private fun init() {
+        btnCapture = findViewById(R.id.btnCapture)
+        arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment?
+        addNode()
+    }
+
+    private fun generateCacheFilename(): String {
+        val date: String = SimpleDateFormat(FORMAT_TIME, Locale.getDefault()).format(Date())
+        val cacheDir = cacheDir
+        return cacheDir.toString() + File.separator + DOMAIN_URI + date + NAME_SCREEN
+    }
+
+    @Throws(IOException::class)
+    private fun saveBitmapToDisk(bitmap: Bitmap, filename: String) {
+        val out = File(filename)
+        if (out.parentFile?.exists() == false) {
+            out.parentFile?.mkdirs()
+        }
+        try {
+            FileOutputStream(filename).use { outputStream ->
+                ByteArrayOutputStream().use { outputData ->
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputData)
+                    outputData.writeTo(outputStream)
+                    outputStream.flush()
+                    outputStream.close()
+                }
+            }
+        } catch (ex: IOException) {
+            throw IOException("Failed to save bitmap to disk", ex)
+        }
+    }
+
+    private fun takePhoto() {
+        val filename = generateCacheFilename()
+        val view: ArSceneView = arFragment!!.arSceneView
+        val bitmap = Bitmap.createBitmap(
+            view.width, view.height,
+            Bitmap.Config.ARGB_8888
+        )
+
+        val handlerThread = HandlerThread("PixelCopier")
+        handlerThread.start()
+        PixelCopy.request(view, bitmap, { copyResult ->
+            if (copyResult == PixelCopy.SUCCESS) {
+                try {
+                    saveBitmapToDisk(bitmap, filename)
+                    runOnUiThread {
+//                        addImageToNode(filename)
+                    }
+
+                } catch (e: IOException) {
+                    val toast = Toast.makeText(
+                        this@MainActivity, e.toString(),
+                        Toast.LENGTH_LONG
+                    )
+                    toast.show()
+                    return@request
+                }
+            } else {
+                //noop
+            }
+            handlerThread.quitSafely()
+        }, Handler(handlerThread.looper))
+
+    }
 
     private fun checkIsSupportedDeviceOrFinish(activity: Activity): Boolean {
         val openGlVersionString = (activity.getSystemService(ACTIVITY_SERVICE) as ActivityManager)
             .deviceConfigurationInfo
             .glEsVersion
         if (openGlVersionString.toDouble() < MIN_OPENGL_VERSION) {
-            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later")
+            Log.e(TAG, "Scene-form requires OpenGL ES 3.0 later")
             activity.finish()
             return false
         }
@@ -121,12 +139,12 @@ class MainActivity : AppCompatActivity() {
         val width = 2.0
 
         val positions = listOf(
-            Vector3(0.0f, 0f, -distance.toFloat()),
-            Vector3((3.0 * width / 4.0).toFloat(), 0f, (-distance / 2.0).toFloat()),
-            Vector3((3.0 * width / 4.0).toFloat(), 0f, (distance / 2.0).toFloat()),
-            Vector3(0.0f, 0f, distance.toFloat()),
-            Vector3((-3.0 * width / 4.0).toFloat(), 0f, (distance / 2.0).toFloat()),
-            Vector3((-3.0 * width / 4.0).toFloat(), 0f, (-distance / 2.0).toFloat()),
+            Vector3(0.0f, -1f, -distance.toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), -1f, (-distance / 2.0).toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), -1f, (distance / 2.0).toFloat()),
+            Vector3(0.0f, -1f, distance.toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), -1f, (distance / 2.0).toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), -1f, (-distance / 2.0).toFloat()),
         )
 
         val rotations = listOf(
@@ -139,12 +157,12 @@ class MainActivity : AppCompatActivity() {
         )
 
         val imageResourcesMid = arrayOf(
-            R.drawable.img1,
-            R.drawable.img2,
-            R.drawable.img3,
-            R.drawable.img4,
-            R.drawable.img5,
-            R.drawable.img6
+            R.drawable.img_2,
+            R.drawable.img_2,
+            R.drawable.img_2,
+            R.drawable.img_2,
+            R.drawable.img_2,
+            R.drawable.img_2
         )
 
         for (i in 0 until 6) {
@@ -153,7 +171,9 @@ class MainActivity : AppCompatActivity() {
             Glide.with(this)
                 .load(imageResourcesMid[i % imageResourcesMid.size])
                 .apply(RequestOptions().centerCrop())
+                .override(250, 250)
                 .into(imageView)
+
 
 
             ViewRenderable.builder()
@@ -162,7 +182,7 @@ class MainActivity : AppCompatActivity() {
                 .build()
                 .thenAccept { viewRenderer ->
                     val anchorNode = Node()
-                    anchorNode.name = "mid_node$i"
+                    anchorNode.name = "mid_node_$i"
                     anchorNode.localPosition = positions[i]
                     arFragment?.arSceneView?.scene?.addChild(anchorNode)
                     anchorNode.localRotation = rotations[i]
@@ -174,25 +194,22 @@ class MainActivity : AppCompatActivity() {
                 }
         }
 
-        val topY = (width / 2 + sqrt(2.0) / 2).toFloat()
-
-
         val topPositions = listOf(
-            Vector3(0.0f, topY, -distance.toFloat()),
-            Vector3((3.0 * width / 4.0).toFloat(), topY, (-distance / 2.0).toFloat()),
-            Vector3((3.0 * width / 4.0).toFloat(), topY, (distance / 2.0).toFloat()),
-            Vector3(0.0f, topY, distance.toFloat()),
-            Vector3((-3.0 * width / 4.0).toFloat(), topY, (distance / 2.0).toFloat()),
-            Vector3((-3.0 * width / 4.0).toFloat(), topY, (-distance / 2.0).toFloat()),
+            Vector3(0.0f, 1f, -distance.toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), 1f, (-distance / 2.0).toFloat()),
+            Vector3((3.0 * width / 4.0).toFloat(), 1f, (distance / 2.0).toFloat()),
+            Vector3(0.0f, 1f, distance.toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), 1f, (distance / 2.0).toFloat()),
+            Vector3((-3.0 * width / 4.0).toFloat(), 1f, (-distance / 2.0).toFloat()),
         )
 
         val topRotations = listOf(
             Quaternion(0f, 1f, PI.toFloat() / 4f, 0f),
-            Quaternion(0f, 1f, -PI.toFloat() / 4f, -PI.toFloat() / 2f),
-            Quaternion(-PI.toFloat() / 5f, 1f, -PI.toFloat() / 4f, -2 * PI.toFloat() / 8f),
+            Quaternion(-PI.toFloat() / 3f, 1f, -PI.toFloat() / 4f, -PI.toFloat() / 2f),
+            Quaternion(-PI.toFloat() / 6f, 1f, -PI.toFloat() / 4f, -2 * PI.toFloat() / 8f),
             Quaternion(0f, 1f, -PI.toFloat() / 4f, 0f),
-            Quaternion(0f, 1f, -PI.toFloat() / 4f, 2 * PI.toFloat() / 8f),
-            Quaternion(0f, 1f, -PI.toFloat() / 4f, PI.toFloat() / 2f),
+            Quaternion(PI.toFloat() / 6f, 1f, -PI.toFloat() / 4f, 2 * PI.toFloat() / 8f),
+            Quaternion(PI.toFloat() / 2f, 1f, -PI.toFloat() / 4f, PI.toFloat() / 2f),
         )
 
         for (i in 0 until 6) {
@@ -201,8 +218,8 @@ class MainActivity : AppCompatActivity() {
             Glide.with(this)
                 .load(imageResourcesMid[i % imageResourcesMid.size])
                 .apply(RequestOptions().centerCrop())
+                .override(250, 250)
                 .into(imageView)
-
 
             ViewRenderable.builder()
                 .setView(this, imageView)
@@ -210,11 +227,12 @@ class MainActivity : AppCompatActivity() {
                 .build()
                 .thenAccept { viewRenderer ->
                     val anchorNode = Node()
-                    anchorNode.name = "top_node$i"
+                    anchorNode.name = "top_node_$i"
                     anchorNode.localPosition = topPositions[i]
                     arFragment?.arSceneView?.scene?.addChild(anchorNode)
                     anchorNode.localRotation = topRotations[i]
                     anchorNode.renderable = viewRenderer
+
                 }
                 .exceptionally {
                     Log.e(TAG, "$it")
@@ -224,7 +242,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        val bottomY = -(width / 2 + sqrt(2.0) / 2).toFloat()
+        val bottomY = -3f
         val bottomPositions = listOf(
             Vector3(0.0f, bottomY, -distance.toFloat()),
             Vector3((3.0 * width / 4.0).toFloat(), bottomY, (-distance / 2.0).toFloat()),
@@ -248,8 +266,8 @@ class MainActivity : AppCompatActivity() {
             Glide.with(this)
                 .load(imageResourcesMid[i % imageResourcesMid.size])
                 .apply(RequestOptions().centerCrop())
+                .override(250, 250)
                 .into(imageView)
-
 
             ViewRenderable.builder()
                 .setView(this, imageView)
@@ -257,7 +275,7 @@ class MainActivity : AppCompatActivity() {
                 .build()
                 .thenAccept { viewRenderer ->
                     val anchorNode = Node()
-                    anchorNode.name = "bottom_node$i"
+                    anchorNode.name = "bottom_node_$i"
                     anchorNode.localPosition = bottomPositions[i]
                     arFragment?.arSceneView?.scene?.addChild(anchorNode)
                     anchorNode.localRotation = bottomRotations[i]
@@ -269,5 +287,13 @@ class MainActivity : AppCompatActivity() {
                 }
 
         }
+    }
+
+    private companion object {
+        const val TAG = "longtq"
+        const val MIN_OPENGL_VERSION = 3.0
+        const val FORMAT_TIME = "yyyyMMddHHmm_ss"
+        const val NAME_SCREEN = "_screenshot.jpg"
+        const val DOMAIN_URI = "IM/"
     }
 }
